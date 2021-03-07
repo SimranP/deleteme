37834d9deb40466a0555254fd0d9bc6ed2dc1398
Simran <simranpalmeerut@gmail.com>
2021-03-07 15:24:43 +0530

    Added calculator

---
+++ src/src/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+  public Calculator() {
+  }
+  public Integer add(int a, int b){
+    return a + b;
+  }
+  public Integer sub(int a, int b){
+    return a - b;
+  }
+}
--- src/src/HelloWorld.java
+++ src/src/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    Calculator calculator = new Calculator();
+    System.out.println("Hello! I am a calculator");
+    System.out.println("2 + 2 = " + calculator.add(2, 2));
+    System.out.println("2 - 2 = " + calculator.sub(2, 2));
   }
 }
